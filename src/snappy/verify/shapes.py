f2a2c90f1b34c778f03b2c1bf40ab78b2b481ab6
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2026-05-08 10:19:30 -0700

    compute_hyperbolic_shapes: handling non-orientable manifolds.

--- src/snappy/verify/shapes.py
+++ src/snappy/verify/shapes.py
@@ -4,8 +4,9 @@ __all__ = ['compute_hyperbolic_shapes']
 
 from typing import Optional
 
-def compute_hyperbolic_shapes(manifold,
-                              verified : bool, bits_prec : Optional[int] = None):
+def compute_hyperbolic_shapes_oriented(
+        manifold, *,
+        verified : bool, bits_prec : Optional[int] = None):
 
     # Get shapes, as intervals if requested
     shapes = manifold.tetrahedra_shapes('rect', intervals=verified,
@@ -23,3 +24,23 @@ def compute_hyperbolic_shapes(manifold,
                 "Manifold has non-geometric solution type '%s'." % sol_type)
 
     return shapes
+
+def compute_hyperbolic_shapes(
+        manifold, *,
+        verified : bool, bits_prec : Optional[int] = None):
+
+    if verified or bits_prec is not None:
+        if manifold.is_orientable():
+            return compute_hyperbolic_shapes_oriented(
+                manifold,
+                verified = verified, bits_prec = bits_prec)
+        else:
+            return compute_hyperbolic_shapes_oriented(
+                manifold.orientation_cover(),
+                verified = verified, bits_prec = bits_prec)[::2]
+    else:
+        sol_type = manifold.solution_type()
+        if not sol_type == 'all tetrahedra positively oriented':
+            raise RuntimeError(
+                "Manifold has non-geometric solution type '%s'." % sol_type)
+        return manifold.tetrahedra_shapes('rect')
