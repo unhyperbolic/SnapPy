96ce8c94249bd0e95aed46ddfac9b4e6eccb0b5f
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2026-05-10 20:03:51 -0700

    Adding from . import extensions.

--- src/snappy/__init__.py
+++ src/snappy/__init__.py
@@ -3,6 +3,7 @@
 # logging.basicConfig(filename='example.log',level=logging.DEBUG)
 # logging.debug('This message should go to the log file')
 import sys
+from . import extensions
 from .extensions.SnapPy import (AbelianGroup,
                      FundamentalGroup,
                      SymmetryGroup,
